52c40dddd159a87d75d70a80392a6bd8c9e07f4d
Ivan Fuentes <iv.ifa94@gmail.com>
2023-03-08 17:46:37 -0300

    tercer desafio

---
+++ clase-06/desafio-entregable/src/app.js
@@ -0,0 +1,62 @@
+import ProductManager from "./productManager.js";
+const manager = new ProductManager();
+// Express
+import express from "express";
+import { error } from "console";
+const app = express();
+
+app.get("/products", async (req, res) => {
+  const productsList = await manager.getProducts();
+  const limit = Number(req.query.limit);
+  if (!limit) {
+    res.send(productsList);
+  } else {
+    const productListWithLimit = productsList.slice(0, limit);
+    res.send(productListWithLimit);
+  }
+});
+
+app.get("/products/:pid", async (req, res) => {
+  let id = req.params.pid;
+  let productById = await manager.getProductById(Number(id));
+
+  res.send(productById);
+});
+
+app.listen(8080, () => {
+  console.log("Servidor arriba en el puerto 8080");
+});
+
+//const env = async () => {
+/*  let prueba1 = await manager.getProducts();
+  console.log(prueba1); */
+
+/* let objeto2 = await manager.addProduct(
+    "Banana de prueba",
+    "esta es una Banana de prueba",
+    200,
+    "sin imagen",
+    "Banana123",
+    25
+  );
+  console.log(objeto2); */
+
+/* let prueba2 = await manager.getProductById(3);
+  console.log(prueba2); */
+
+/*   let prueba3 = await manager.updateProduct(
+    3,
+    "melon",
+    "esta es un melon de prueba",
+    300,
+    "sin imagen",
+    "mln123",
+    40
+  );
+  console.log(prueba3); */
+
+/* let prueba4 = await manager.deleteProduct(1);
+  console.log(prueba4); */
+//};
+
+//env();
---
+++ clase-06/desafio-entregable/src/productManager.js
@@ -0,0 +1,113 @@
+import fs from "fs";
+
+export default class ProductManager {
+  constructor() {
+    this.products = [];
+    this.path = "files/productManager.json";
+  }
+  getProducts = async () => {
+    if (fs.existsSync(this.path)) {
+      const data = await fs.promises.readFile(this.path, "utf-8");
+      const result = JSON.parse(data);
+      return result;
+    } else {
+      return [];
+    }
+  };
+  addProduct = async (title, description, price, thumbnail, code, stock) => {
+    const products = await this.getProducts();
+    if ((!title, !description, !price, !thumbnail, !code, !stock)) {
+      console.error("Please complete all product fields.");
+      return;
+    } else {
+      if (this.#codeIndex(products, code) === -1) {
+        const product = {
+          title,
+          description,
+          price,
+          thumbnail,
+          code,
+          stock,
+          id: products.length + 1,
+        };
+        products.push(product);
+        await fs.promises.writeFile(
+          this.path,
+          JSON.stringify(products, null, "\t")
+        );
+        return products;
+      } else {
+        console.error(`Product with code : "${code}" already exists`);
+        return;
+      }
+    }
+  };
+  #codeIndex = (array, code) => {
+    const codeIndex = array.findIndex((product) => product.code === code);
+    return codeIndex;
+  };
+  findProduct = async (productId) => {
+    if (fs.existsSync(this.path)) {
+      const data = await fs.promises.readFile(this.path, "utf-8");
+      const result = JSON.parse(data);
+
+      const productIndex = result.findIndex(
+        (product) => product.id === productId
+      );
+      if (productIndex === -1) {
+        console.error(`The Product with ID : "${productId}" does not exist.`);
+        return productIndex;
+      } else {
+        return productIndex;
+      }
+    }
+  };
+  getProductById = async (productId) => {
+    const search = await this.findProduct(productId);
+    const products = await this.getProducts();
+    return products[search];
+  };
+  deleteProduct = async (productId) => {
+    const search = await this.findProduct(productId);
+    const products = await this.getProducts();
+
+    if (search === -1) {
+      return;
+    } else {
+      products.splice(search, 1);
+      await fs.promises.writeFile(
+        this.path,
+        JSON.stringify(products, null, "\t")
+      );
+      return console.log("Product deleted successfully !");
+    }
+  };
+
+  updateProduct = async (
+    id,
+    title,
+    description,
+    price,
+    thumbnail,
+    code,
+    stock
+  ) => {
+    const search = await this.findProduct(id);
+    const products = await this.getProducts();
+    const product = {
+      title: title ?? result[search].title,
+      description: description ?? result[search].description,
+      price: price ?? result[search].price,
+      thumbnail: thumbnail ?? result[search].thumbnail,
+      code: code ?? result[search].code,
+      stock: stock ?? result[search].stock,
+      id: id,
+    };
+    products[search] = product;
+    await fs.promises.writeFile(
+      this.path,
+      JSON.stringify(products, null, "\t")
+    );
+    return console.log("Product updated successfully !");
+  };
+}
